22565dce994b6dcdf5e305da7cc1e1db9db34f14
miyad <mhmiyad6565@gmail.com>
2023-01-24 15:26:51 +0600

    adding slider

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./public/index.html",
+           
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+   
+  ],
+}
